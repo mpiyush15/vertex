4fd53c079e1d5aebb712917784f9142f76038afc
piyush <pixelsadvertise@gmail.com>
2025-04-03 22:45:17 +0530

    change named

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
    <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p className="text-lg">This is a simple Next.js app with Tailwind CSS.</p>
+      <p className="text-lg">Vertex Science Point</p>
       <Image
         src="/images/nextjs.png"
         alt="Next.js Logo"
